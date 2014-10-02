5fcbbfa1f9c912d2b13a33c85a398617f286d88d
Paul Loizeaux <paulloizeaux@gmail.com>
2014-10-02 12:38:55 -0400

    Correct indentation on SuperActivity

--- DroidPlanner/src/com/droidplanner/activitys/helpers/SuperActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/helpers/SuperActivity.java
@@ -45,253 +45,253 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 
 public abstract class SuperActivity extends Activity implements
-							 OnNavigationListener, ConnectionStateListner, OnAltitudeChangedListner, OnSystemArmListener{
+OnNavigationListener, ConnectionStateListner, OnAltitudeChangedListner, OnSystemArmListener{
 
-    public abstract int getNavigationItem();
+	public abstract int getNavigationItem();
 
-    public DroidPlannerApp app;
-    public Drone drone;
-    private MenuItem connectButton;
-    private MenuItem armButton;
+	public DroidPlannerApp app;
+	public Drone drone;
+	private MenuItem connectButton;
+	private MenuItem armButton;
 
-    private ScreenOrientation screenOrientation = new ScreenOrientation(this);
+	private ScreenOrientation screenOrientation = new ScreenOrientation(this);
 
-    private final int[] armSafeScreens = {1,4,5,6};
-    public SuperActivity() {
-	super();
-    }
+	private final int[] armSafeScreens = {1,4,5,6};
+	public SuperActivity() {
+		super();
+	}
 
-    @Override
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 
-	PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
-	// Set up the action bar to show a dropdown list.
-	setUpActionBar();
-	app = (DroidPlannerApp) getApplication();
-	app.conectionListner = this;
-	app.onSystemArmListener = this;
-	this.drone = app.drone;
+		// Set up the action bar to show a dropdown list.
+		setUpActionBar();
+		app = (DroidPlannerApp) getApplication();
+		app.conectionListner = this;
+		app.onSystemArmListener = this;
+		this.drone = app.drone;
 
-	setVolumeControlStream(AudioManager.STREAM_MUSIC);
-	screenOrientation.unlock();
-    }
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		screenOrientation.unlock();
+	}
 
-    public void setUpActionBar() {
-	final ActionBar actionBar = getActionBar();
-	actionBar.setDisplayShowTitleEnabled(false);
-	actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-	SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this,
-									 R.array.menu_dropdown,
-									 android.R.layout.simple_spinner_dropdown_item);
-	actionBar.setListNavigationCallbacks(mSpinnerAdapter, this);
-	actionBar.setSelectedNavigationItem(getNavigationItem());
-    }
+	public void setUpActionBar() {
+		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this,
+				R.array.menu_dropdown,
+				android.R.layout.simple_spinner_dropdown_item);
+		actionBar.setListNavigationCallbacks(mSpinnerAdapter, this);
+		actionBar.setSelectedNavigationItem(getNavigationItem());
+	}
 
-    /**
-     * Used to lock the user into 
-     */
-    @Override
+	/**
+	 * Used to lock the user into 
+	 */
+	@Override
 	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-		
-	if (itemPosition == getNavigationItem()) {
-	    return false;
+
+		if (itemPosition == getNavigationItem()) {
+			return false;
+		}
+
+		if(!isSafeScreen(itemPosition)){
+			Toast.makeText(this, "Unable to access selected screen when vehicle is armed", Toast.LENGTH_LONG).show();
+			return false;
+		}
+
+		Intent navigationIntent = new Intent(this, FlightDataActivity.class);
+
+		switch (itemPosition) {
+
+		case 0: // Planning
+			navigationIntent = new Intent(this, PlanningActivity.class);
+			break;
+		default:
+
+		case 1: // Flight Data
+			navigationIntent = new Intent(this, FlightDataActivity.class);
+			navigationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			break;
+
+		case 2: // RC
+			navigationIntent = new Intent(this, RCActivity.class);
+			break;
+
+		case 3: // Parameters
+			navigationIntent = new Intent(this, ParametersActivity.class);
+			break;
+
+		case 4: // Camera
+			navigationIntent = new Intent(this, CameraActivity.class);
+			break;
+
+		case 5: // GCP
+			navigationIntent = new Intent(this, GCPActivity.class);
+			break;
+
+		case 6: // Chart
+			navigationIntent = new Intent(this, ChartActivity.class);
+			break;
+		}
+
+		startActivity(navigationIntent);
+
+		return false;
+
 	}
 
-	if(!isSafeScreen(itemPosition)){
-	    Toast.makeText(this, "Unable to access selected screen when vehicle is armed", Toast.LENGTH_LONG).show();
-	    return false;
-	}
-	
-	Intent navigationIntent = new Intent(this, FlightDataActivity.class);
-	
-	switch (itemPosition) {
-	
-	case 0: // Planning
-	    navigationIntent = new Intent(this, PlanningActivity.class);
-	    break;
-	default:
-	
-	case 1: // Flight Data
-	    navigationIntent = new Intent(this, FlightDataActivity.class);
-	    navigationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	    break;
-	
-	case 2: // RC
-	    navigationIntent = new Intent(this, RCActivity.class);
-	    break;
-	
-	case 3: // Parameters
-	    navigationIntent = new Intent(this, ParametersActivity.class);
-	    break;
-	
-	case 4: // Camera
-	    navigationIntent = new Intent(this, CameraActivity.class);
-	    break;
-	
-	case 5: // GCP
-	    navigationIntent = new Intent(this, GCPActivity.class);
-	    break;
-	
-	case 6: // Chart
-	    navigationIntent = new Intent(this, ChartActivity.class);
-	    break;
-	}
-	
-	startActivity(navigationIntent);
-	
-	return false;
-    
-    }
-
-    @Override
+	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
-	switch (item.getItemId()) {
-	case R.id.menu_settings:
-	    startActivity(new Intent(this, SettingsActivity.class));
-	    return true;
-	case R.id.menu_connect:
-	    drone.MavClient.toggleConnectionState();
-	    return true;
-	case R.id.menu_load_from_apm:
-	    drone.waypointMananger.getWaypoints();
-	    return true;
-	case R.id.menu_default_alt:
-	    changeDefaultAlt();
-	    return true;
-	case R.id.menu_preflight_calibration:
-	    drone.calibrationSetup.startCalibration(this);
-	    return true;
-	case R.id.menu_record_me:
-	    app.recordMe.toogleRecordMeState();
-	    return true;
-	case R.id.menu_follow_me:
-	    app.followMe.toogleFollowMeState();
-	    return true;
-	case R.id.menu_preflight_checklist:
-	    showCheckList();
-	    return true;
-	case R.id.menu_map_type_hybrid:
-	case R.id.menu_map_type_normal:
-	case R.id.menu_map_type_terrain:
-	case R.id.menu_map_type_satellite:
-	    setMapTypeFromItemId(item.getItemId());
-	    return true;
-	default:
-	    return super.onMenuItemSelected(featureId, item);
-	}
-    }
-
-    private void showCheckList() {
-	PreflightDialog dialog = new PreflightDialog();
-	dialog.build(this, drone, false);
-
-    }
-
-    private void setMapTypeFromItemId(int itemId) {
-
-	final String mapType;
-	switch(itemId) {
-	case R.id.menu_map_type_hybrid:
-	    mapType = OfflineMapFragment.MAP_TYPE_HYBRID;
-	    break;
-	case R.id.menu_map_type_normal:
-	    mapType = OfflineMapFragment.MAP_TYPE_NORMAL;
-	    break;
-	case R.id.menu_map_type_terrain:
-	    mapType = OfflineMapFragment.MAP_TYPE_TERRAIN;
-	    break;
-	default:
-	    mapType = OfflineMapFragment.MAP_TYPE_SATELLITE;
-	    break;
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+		case R.id.menu_connect:
+			drone.MavClient.toggleConnectionState();
+			return true;
+		case R.id.menu_load_from_apm:
+			drone.waypointMananger.getWaypoints();
+			return true;
+		case R.id.menu_default_alt:
+			changeDefaultAlt();
+			return true;
+		case R.id.menu_preflight_calibration:
+			drone.calibrationSetup.startCalibration(this);
+			return true;
+		case R.id.menu_record_me:
+			app.recordMe.toogleRecordMeState();
+			return true;
+		case R.id.menu_follow_me:
+			app.followMe.toogleFollowMeState();
+			return true;
+		case R.id.menu_preflight_checklist:
+			showCheckList();
+			return true;
+		case R.id.menu_map_type_hybrid:
+		case R.id.menu_map_type_normal:
+		case R.id.menu_map_type_terrain:
+		case R.id.menu_map_type_satellite:
+			setMapTypeFromItemId(item.getItemId());
+			return true;
+		default:
+			return super.onMenuItemSelected(featureId, item);
+		}
 	}
 
-	PreferenceManager.getDefaultSharedPreferences(this).edit()
-	    .putString(OfflineMapFragment.PREF_MAP_TYPE, mapType)
-	    .commit();
+	private void showCheckList() {
+		PreflightDialog dialog = new PreflightDialog();
+		dialog.build(this, drone, false);
 
-	drone.notifyMapTypeChanged();
-    }
-
-    public void notifyDisconnected() {
-	if (connectButton != null) {
-	    connectButton.setTitle(getResources().getString(
-							    R.string.menu_connect));
-	}
-	if(armButton != null){
-	    armButton.setEnabled(false);
 	}
 
-	// Reset launch failsafe on disconnect
-	drone.waypointsSynced = false;
+	private void setMapTypeFromItemId(int itemId) {
 
-	screenOrientation.unlock();
-    }
+		final String mapType;
+		switch(itemId) {
+		case R.id.menu_map_type_hybrid:
+			mapType = OfflineMapFragment.MAP_TYPE_HYBRID;
+			break;
+		case R.id.menu_map_type_normal:
+			mapType = OfflineMapFragment.MAP_TYPE_NORMAL;
+			break;
+		case R.id.menu_map_type_terrain:
+			mapType = OfflineMapFragment.MAP_TYPE_TERRAIN;
+			break;
+		default:
+			mapType = OfflineMapFragment.MAP_TYPE_SATELLITE;
+			break;
+		}
 
-    public void notifyConnected() {
-	if (connectButton != null) {
-	    connectButton.setTitle(getResources().getString(
-							    R.string.menu_disconnect));
+		PreferenceManager.getDefaultSharedPreferences(this).edit()
+		.putString(OfflineMapFragment.PREF_MAP_TYPE, mapType)
+		.commit();
+
+		drone.notifyMapTypeChanged();
 	}
-	if(armButton != null){
-	    armButton.setEnabled(true);
+
+	public void notifyDisconnected() {
+		if (connectButton != null) {
+			connectButton.setTitle(getResources().getString(
+					R.string.menu_connect));
+		}
+		if(armButton != null){
+			armButton.setEnabled(false);
+		}
+
+		// Reset launch failsafe on disconnect
+		drone.waypointsSynced = false;
+
+		screenOrientation.unlock();
 	}
-	screenOrientation.requestLock();
-    }
 
-    public void notifyArmed() {
-
-	// Change menu button
-	if (armButton != null) {
-	    armButton.setTitle("Disarm");
+	public void notifyConnected() {
+		if (connectButton != null) {
+			connectButton.setTitle(getResources().getString(
+					R.string.menu_disconnect));
+		}
+		if(armButton != null){
+			armButton.setEnabled(true);
+		}
+		screenOrientation.requestLock();
 	}
-    }
 
-    public void notifyDisarmed() {
+	public void notifyArmed() {
 
-	// Change menu button
-	if (armButton != null) {
-	    armButton.setTitle("Arm");
+		// Change menu button
+		if (armButton != null) {
+			armButton.setTitle("Disarm");
+		}
 	}
-    }
-    @Override
+
+	public void notifyDisarmed() {
+
+		// Change menu button
+		if (armButton != null) {
+			armButton.setTitle("Arm");
+		}
+	}
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	getMenuInflater().inflate(R.menu.menu_super_activiy, menu);
-	armButton = menu.findItem(R.id.menu_arm);
-	connectButton = menu.findItem(R.id.menu_connect);
-	drone.MavClient.queryConnectionState();
-	return super.onCreateOptionsMenu(menu);
-    }
+		getMenuInflater().inflate(R.menu.menu_super_activiy, menu);
+		armButton = menu.findItem(R.id.menu_arm);
+		connectButton = menu.findItem(R.id.menu_connect);
+		drone.MavClient.queryConnectionState();
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    public void changeDefaultAlt() {
-	AltitudeDialog dialog = new AltitudeDialog(this);
-	dialog.build(drone.mission.getDefaultAlt(), this);
-    }
+	public void changeDefaultAlt() {
+		AltitudeDialog dialog = new AltitudeDialog(this);
+		dialog.build(drone.mission.getDefaultAlt(), this);
+	}
 
-    @Override
+	@Override
 	public void onAltitudeChanged(double newAltitude,boolean applyToAll) {
-	drone.mission.setDefaultAlt(newAltitude);
+		drone.mission.setDefaultAlt(newAltitude);
 
-	if(applyToAll){
-	    changeAllAltitudes(newAltitude);
-	}
-    }
-
-    public void changeAllAltitudes(double newAltitude){
-	List<LatLng> pathPoints = drone.mission.getPathPoints();
-	drone.mission.clearWaypoints();
-	for(LatLng wPoint:pathPoints){
-	    drone.mission.addWaypoint(wPoint.latitude,
-				      wPoint.longitude, newAltitude);
+		if(applyToAll){
+			changeAllAltitudes(newAltitude);
+		}
 	}
 
-    }
+	public void changeAllAltitudes(double newAltitude){
+		List<LatLng> pathPoints = drone.mission.getPathPoints();
+		drone.mission.clearWaypoints();
+		for(LatLng wPoint:pathPoints){
+			drone.mission.addWaypoint(wPoint.latitude,
+					wPoint.longitude, newAltitude);
+		}
 
-    public boolean isSafeScreen(int screenIndex){
-	if(!drone.state.isArmed())
-	    return true;
+	}
 
-	else return Arrays.asList(armSafeScreens).contains(screenIndex);
-    }
+	public boolean isSafeScreen(int screenIndex){
+		if(!drone.state.isArmed())
+			return true;
+
+		else return Arrays.asList(armSafeScreens).contains(screenIndex);
+	}
 }
